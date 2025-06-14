787e33d4f88aadbf2aeef86029831df381c1c3b9
YCheng <briansam195@gmail.com>
2025-06-14 23:52:26 +0800

    商品功能 - 實作查詢商品列表 (分頁)

--- src/main/java/com/example/springboot_mall/dao/impl/ProductImpl.java
+++ src/main/java/com/example/springboot_mall/dao/impl/ProductImpl.java
@@ -27,6 +27,7 @@ public class ProductImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if (productQueryParams.getCategory() != null) {
             sql += " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -37,7 +38,13 @@ public class ProductImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch()  + "%");
         }
 
+        // 排序
         sql += " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        // 分頁
+        sql += " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         return namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
     }
